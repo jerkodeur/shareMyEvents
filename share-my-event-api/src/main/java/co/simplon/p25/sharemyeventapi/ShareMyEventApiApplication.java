f58dce41ed96ea89c9316843d025dce1156ad096
jerkoLegion <jerome.potie@gmail.com>
2022-03-27 10:31:09 +0200

    Create SpringBoot application

---
+++ share-my-event-api/src/main/java/co/simplon/p25/sharemyeventapi/ShareMyEventApiApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.p25.sharemyeventapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareMyEventApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareMyEventApiApplication.class, args);
+	}
+
+}
---
+++ share-my-event-api/src/test/java/co/simplon/p25/sharemyeventapi/ShareMyEventApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.simplon.p25.sharemyeventapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareMyEventApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
