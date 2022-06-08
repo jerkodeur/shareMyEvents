41e1c89472d23815b2d39afd615b48b8a96149e0
jerkoLegion <jerome.potie@gmail.com>
2022-06-08 15:46:05 +0200

    change jwt authorized domain route

--- share-my-events-angular/src/environments/environment.prod.ts
+++ share-my-events-angular/src/environments/environment.prod.ts
@@ -1,5 +1,11 @@
 export const environment = {
   production: true,
   apiUrl: 'https://gateway.readresolve.tech:9443/share',
-  apiBaseUrl: 'readresolve.tech',
+  apiBaseUrl: [
+    'readresolve.tech',
+    'share.readresolve.tech',
+    'www.readresolve.tech',
+    'gateway.readresolve.tech',
+    'https://gateway.readresolve.tech:9443/share',
+  ],
 };
