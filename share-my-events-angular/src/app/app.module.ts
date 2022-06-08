8f1bdc480f336a1eb3a75cceddff35dd04f4af75
jerkoLegion <jerome.potie@gmail.com>
2022-06-08 15:24:53 +0200

    change jwt authorized domain route

--- share-my-events-angular/src/app/app.module.ts
+++ share-my-events-angular/src/app/app.module.ts
@@ -114,7 +114,7 @@ export function tokenGetter() {
     JwtModule.forRoot({
       config: {
         tokenGetter: tokenGetter,
-        allowedDomains: [`${environment.apiUrl}/`],
+        allowedDomains: [`${environment.apiBaseUrl}`],
         disallowedRoutes: [
           `${environment.apiUrl}/users/login`,
           `${environment.apiUrl}/users/signup`,
