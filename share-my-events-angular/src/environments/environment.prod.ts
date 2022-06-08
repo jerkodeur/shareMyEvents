18b6bc94bd8a4275db467f4028c332a0633a3181
jerkoLegion <jerome.potie@gmail.com>
2022-06-08 19:26:46 +0200

    test fix add participant to event

--- share-my-events-angular/src/environments/environment.prod.ts
+++ share-my-events-angular/src/environments/environment.prod.ts
@@ -1,14 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://gateway.readresolve.tech:9443/share',
-  apiBaseUrl: [
-    // 'readresolve.tech',
-    // 'share.readresolve.tech',
-    // 'www.readresolve.tech',
-    // 'https://gateway.readresolve.tech:9443',
-    // 'gateway.readresolve.tech:9443/share',
-    'gateway.readresolve.tech:9443',
-    // 'localhost:9443',
-    // 'localhost:9093',
-  ],
+  apiBaseUrl: ['gateway.readresolve.tech:9443'],
 };
